61e510b48e1976c643338efe52befbae125c4fae
rajib <rajibbapy@gmail.com>
2019-03-06 23:52:30 +0600

    Database Connection

---
+++ adddonarwindow.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace bloodbank
+{
+    public partial class adddonarwindow : UserControl
+    {
+        // Db Connection
+        private void DBConnection()
+        {
+            string ConnectString = "datasource = localhost; username = root; password = ; database = blood_bank";
+            MySqlConnection DBConnect = new MySqlConnection(ConnectString);
+            try
+            {
+                DBConnect.Open();
+                MessageBox.Show("Ok You are Connected");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        public adddonarwindow()
+        {
+            InitializeComponent();
+        }
+
+        private void savebtn_Click(object sender, EventArgs e)
+        {
+            DBConnection();
+        }
+        
+
+    }
+}
