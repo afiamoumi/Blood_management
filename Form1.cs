5021763df41b7542defcb201def0e7cef96ebd10
rajib <rajibbapy@gmail.com>
2019-03-06 23:44:39 +0600

    Moving the Window X-axis and Y-axis

--- Form1.cs
+++ Form1.cs
@@ -13,6 +13,10 @@ namespace bloodbank
 {
     public partial class Form1 : Form
     {
+        int TogMove;
+        int MValX;
+        int MValY;
+
         public Form1()
         {
             InitializeComponent();
@@ -37,6 +41,25 @@ namespace bloodbank
             sideActivePanel.Height = donorListbtn.Height;
             sideActivePanel.Top = donorListbtn.Top;
             donarlistwindow1.BringToFront();
+        }
+        private void panel2_MouseDown(object sender, MouseEventArgs e)
+        {
+            TogMove = 1;
+            MValX = e.X;
+            MValY = e.Y;
+        }
+
+        private void panel2_MouseUp(object sender, MouseEventArgs e)
+        {
+            TogMove = 0;
+        }
+
+        private void panel2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (TogMove == 1)
+            {
+                this.SetDesktopLocation(MousePosition.X - MValX, MousePosition.Y - MValY);
+            }
         }
     }
 }
