1c9093441880abcff738917eff5ff3f0193d7c27
rajib <rajibbapy@gmail.com>
2019-03-06 23:33:45 +0600

    Home Click Event With Active Icon

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using bloodbank.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bloodbank
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            sideActivePanel.Height = homebtn.Height;
+            homewindow1.BringToFront();
+        }
+
+        private void homebtn_Click(object sender, EventArgs e)
+        {
+            sideActivePanel.Height = homebtn.Height;
+            sideActivePanel.Top = homebtn.Top;
+            homewindow1.BringToFront();
+        }
+    }
+}
