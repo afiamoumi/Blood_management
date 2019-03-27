cfef747bd6cd5d3c5fe3635e1edb3456c4404b71
rashedrafin <47659588+rashedrafin@users.noreply.github.com>
2019-03-28 00:51:35 +0600

    Profile Image
    Image upload for pforile

--- adddonarwindow.cs
+++ adddonarwindow.cs
@@ -7,27 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
 
 namespace bloodbank
 {
     public partial class adddonarwindow : UserControl
     {
-        // Db Connection
-        private void DBConnection()
-        {
-            string ConnectString = "datasource = localhost; username = root; password = ; database = blood_bank";
-            MySqlConnection DBConnect = new MySqlConnection(ConnectString);
-            try
-            {
-                DBConnect.Open();
-                MessageBox.Show("Ok You are Connected");
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show(e.Message);
-            }
-        }
 
         public adddonarwindow()
         {
@@ -36,9 +21,65 @@ namespace bloodbank
 
         private void savebtn_Click(object sender, EventArgs e)
         {
-            DBConnection();
-        }
-        
+            //Pickup The Data Form Forum
+            string donorId = donor_id.Text;
+            string donorName = donor_name.Text;
+            string donorAge = donor_age.Text;
+            string donorMobile = mobile_input.Text;
+            string email = email_input.Text;
+            string address = address_input.Text;
+            string lastdonation = last_donation.Value.ToString();
 
+            string connectionString = "Data Source=DESKTOP-SL46HBO\\SQLEXPRESS;Initial Catalog=TestDb;Integrated Security=True";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            // Gender Checked
+            string gender = "";
+            if(male_radiobtn.Checked)
+            {
+                gender = male_radiobtn.Text;
+            }
+            if(female_radiobtn.Checked)
+            {
+                gender = female_radiobtn.Text;
+            }
+            
+            
+
+            string query = "INSERT INTO a VALUES('" + donorId + "', '" + donorName + "', '" + gender + "', '" + donorAge + "', '" + donorMobile + "', '" + email + "',  '" + lastdonation + "', '" + address + "')";
+
+            //Command Execute
+            SqlCommand command = new SqlCommand(query, connection);
+
+            //Connection Open
+            connection.Open();
+
+            int rowAffected = command.ExecuteNonQuery();
+
+            connection.Close();
+
+            if(rowAffected > 0)
+            {
+                MessageBox.Show("Success");
+            }
+            else
+            {
+                MessageBox.Show("Failed");
+            }
+        }
+
+        // Profile Image Upload
+        private void upload_img_btn_Click(object sender, EventArgs e)
+        {
+            string imgLocation = "";
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Imag Files (*.jpg;*.jpeg;.*.gif;)|*.jpg";
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                imgLocation = dialog.FileName.ToString();
+                pictureBox1.ImageLocation = imgLocation;
+            }
+        }
     }
 }
