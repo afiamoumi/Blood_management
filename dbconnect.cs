a79234d2d2ace17e88af8b73d1b13f863be33bc4
rashedrafin <47659588+rashedrafin@users.noreply.github.com>
2019-03-29 09:48:50 +0600

    Database Connect using Universal Db Class

---
+++ dbconnect.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace bloodbank
+{
+    class dbconnect
+    {
+        public static SqlConnection conn = null;
+
+        public void db_connection()
+        {
+            conn = new SqlConnection("Data Source=DESKTOP-SL46HBO\\SQLEXPRESS;Initial Catalog=donorAppDb;Integrated Security=True");
+            conn.Open();
+        }
+    }
+}
